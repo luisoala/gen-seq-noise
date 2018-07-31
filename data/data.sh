da8f6403a208c563652b2d920a995ebfa5b052d4
Donald Duck <luis.oala@online.de>
2018-07-31 12:34:05 +0200

    data bash script

---
+++ data/data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#download zip file with the data
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zo4RDJ6boLHKnHajWMgQ3ty7PsZuJz2U' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zo4RDJ6boLHKnHajWMgQ3ty7PsZuJz2U" -O gen-seq-noise-data.zip && rm -rf /tmp/cookies.txt
+
+#unzip
+unzip gen-seq-noise-data.zip gen-seq-noise-data
+
+#remove zip file
+rm gen-seq-noise-data.zip
+
+#alternatively you can also manually download the data from https://drive.google.com/open?id=1zo4RDJ6boLHKnHajWMgQ3ty7PsZuJz2U
